c34c32e1c6fc58b2bf7aaa9b259496e23ac1b98f
zhenhangtung <zhenhangtung@gmail.com>
2024-11-03 20:49:01 +0800

    chore(insurance): try another prompt

--- scripts/extract_diseases_from_contract.py
+++ scripts/extract_diseases_from_contract.py
@@ -6,8 +6,6 @@ loader = PyPDFLoader(file_path)
 
 docs = loader.load()
 
-print(len(docs))
-
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(model="gpt-4o")
@@ -20,7 +18,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
 vectorstore = InMemoryVectorStore.from_documents(
-    documents=splits, embedding=OpenAIEmbeddings(model="text-embedding-3-large")
+    documents=splits, embedding=OpenAIEmbeddings(model="text-embedding-3-small")
 )
 
 retriever = vectorstore.as_retriever()
@@ -33,11 +31,11 @@ system_prompt = (
     "You are an assistant for question-answering tasks. "
     "Use the following pieces of retrieved context to answer "
     "the question. If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
-    "answer concise."
+    "don't know."
     "\n\n"
     "{context}"
 )
+
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -49,6 +47,9 @@ prompt = ChatPromptTemplate.from_messages(
 question_answer_chain = create_stuff_documents_chain(llm, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-results = rag_chain.invoke({"input": "合同中 7.18 重大疾病 部分下，请找到所有的 101 条重大疾病并将它们以列表形式返回。"})
+results = rag_chain.invoke({"input": "请解析附件中的合同条款，按照以下规则处理完毕后，直接输出结果。从「7.18 重大疾病」中，提取序号（1）至（101）的序号和疾病名称。请注意，所有序号和疾病名称请完整显示，不要做任何省略处理。"})
 
-print(results['answer'])
+for doc in results['context']:
+    print("RAG 的上下文", doc)
+# print("RAG 的上下文", results['context'])
+print("回答", results['answer'])
